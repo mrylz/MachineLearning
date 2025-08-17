023b4c888cacf9c487238f25bf1116e9c41133df
mehmet <m.e.ylmz12345@gmail.com>
2025-08-17 19:18:25 +0300

    rbf svc regression

--- 11YPZ.py
+++ 11YPZ.py
@@ -9,14 +9,12 @@ from lazypredict.Supervised import LazyClassifier,LazyRegressor
 from sklearn.multiclass import  OneVsOneClassifier,OneVsRestClassifier
 from imblearn.over_sampling import SMOTE
 from sklearn.svm import SVC
-data = pd.read_csv('9-email_classification_svm.csv')
+data = pd.read_csv('9-loan_risk_svm.csv')
 print(data.columns)
-sns.scatterplot(x=data["subject_formality_score"],y = data["sender_relationship_score"],hue=data["email_type"])
-plt.show()
-X = data.drop('email_type',axis=1)
-y = data['email_type']
+X = data.drop('loan_risk',axis=1)
+y = data['loan_risk']
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25,random_state=15)
-reg = SVC(kernel="linear")
+reg = SVC(kernel="rbf")
 reg.fit(X_train,y_train)
 y_pred = reg.predict(X_test)
 print(classification_report(y_test,y_pred))
