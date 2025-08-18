b5d742d2dbc8189ef17da6900cc438cf515af28a
mehmet <m.e.ylmz12345@gmail.com>
2025-08-18 12:56:45 +0300

    rbf and 3d scatter

---
+++ 12YPZ2.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split,RandomizedSearchCV,GridSearchCV,StratifiedKFold
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import r2_score,mean_squared_error,mean_absolute_error,accuracy_score,classification_report
+from lazypredict.Supervised import LazyClassifier,LazyRegressor
+from sklearn.multiclass import  OneVsOneClassifier,OneVsRestClassifier
+from imblearn.over_sampling import SMOTE
+from sklearn.svm import SVC
+import plotly.express as px 
+data = pd.read_csv('9-seismic_activity_svm.csv')
+X = data.drop("seismic_event_detected",axis=1)
+y = data["seismic_event_detected"]
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25,random_state=15)
+reg = SVC(kernel="rbf")
+reg.fit(X_train,y_train)
+y_pred = reg.predict(X_test)
+print(classification_report(y_test,y_pred))
